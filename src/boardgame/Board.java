b23357f50bd54270a938dfd6417883eee2b2778c
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-03 12:20:26 -0300

    Chess Layer and printing the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -33,6 +33,12 @@ public class Board {
 		this.colunas = colunas;
 	}
 	
+	public Piece piece(int linha, int coluna) {
+		return pieces[linha][coluna];
+	}
 	
+	public Piece piece(Position position) {
+		return pieces[position.getLinha()][position.getColuna()];
+	}
 	
 }
