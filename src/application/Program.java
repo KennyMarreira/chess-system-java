67bb0a3a207195c34f9c6f8046ddb8fab7376805
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-03 10:54:16 -0300

    Project created

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,6 +6,8 @@ public class Program {
 		
 		System.out.println("Hello World");
 		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
