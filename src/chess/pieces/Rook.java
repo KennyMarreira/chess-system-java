f943500988c28aab1834cc88ce11825203dc36c3
Kenny Marreira <kennymarreira@hotmail.com>
2023-03-06 09:49:12 -0300

    Implementando possiveis movimentos da Torre

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -18,7 +19,52 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean [][] mat = new boolean [getBoard().getLinhas()][getBoard().getColunas()];
+		
+		Position p = new Position(0, 0);
+		
+		// acima
+		p.setValues(position.getLinha() - 1, position.getColuna());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// esquerda
+		p.setValues(position.getLinha(), position.getColuna() -1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() - 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// direita
+		p.setValues(position.getLinha(), position.getColuna() + 1);
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setColuna(p.getColuna() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		// abaixo
+		p.setValues(position.getLinha() + 1, position.getColuna());
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat [p.getLinha()][p.getColuna()] = true;
+			p.setLinha(p.getLinha() + 1);
+		}
+		if (getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+
 		return mat;
 	}
+	
+	
 	
 }
