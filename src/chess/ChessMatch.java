218a39576df63a20d55861b34144890e60b181ad
KennyMarreira <kennymarreira@hotmail.com>
2023-03-07 13:31:14 -0300

    Correcao de excecao na promocao do peao

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -125,7 +124,7 @@ public class ChessMatch {
 			throw new IllegalStateException("Nao ha peca a ser promovida");
 		}
 		if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-			throw new InvalidParameterException("Promocao Invalida");
+			return promoted;
 		}
 		
 		Position pos = promoted.getChessPosition().toPosition();
